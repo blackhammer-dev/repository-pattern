8507030d891e307da0d97993f91cec888728bcfc
darkMode <iransoftnet.ir@gmail.com>
2021-10-25 20:32:09 +0330

    feat: base CRUD

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserUpdateRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(UserRequest $request)
+    {
+        return User::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+    public function update(UserUpdateRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+       return $user;
+    }
+
+    public function destroy($id)
+    {
+        return User::findOrFail($id)->delete();
+    }
+}
