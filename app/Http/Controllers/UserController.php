70a8a97e1272fc3c1cf0d26589efc9c528e48cc5
darkMode <45430743+iransoftnet1@users.noreply.github.com>
2021-10-25 20:52:31 +0330

    feat: search by name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchRequest;
 use App\Http\Requests\UserRequest;
 use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
@@ -14,6 +15,14 @@ class UserController extends Controller
         return User::all();
     }
 
+    public function searchByName(SearchRequest $searchRequest)
+    {
+        return User::where('name', 'like',
+            '%'
+            . $searchRequest->input('query_param')
+            . '%')->get();
+    }
+
     public function store(UserRequest $request)
     {
         return User::create($request->all());
@@ -23,11 +32,12 @@ class UserController extends Controller
     {
         return User::findOrFail($id);
     }
+
     public function update(UserUpdateRequest $request, $id)
     {
         $user = User::findOrFail($id);
         $user->update($request->all());
-       return $user;
+        return $user;
     }
 
     public function destroy($id)
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('user', UserController::class);
 
+Route::get('/user/search/name',[UserController::class,'searchByName']);
