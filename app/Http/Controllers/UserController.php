c8731eb553c6c972b007de0cacfbb60145af4b5e
darkMode <70217785+blackhammer-dev@users.noreply.github.com>
2021-10-29 11:35:46 +0330

    feat: use auto generate facade laravel
    namespace add  Facades\ first use
    With the first class request on the path
    storage / framework / cache / facade-c6f179b . . . .php
    The class is created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,44 +5,37 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SearchRequest;
 use App\Http\Requests\UserRequest;
 use App\Http\Requests\UserUpdateRequest;
-use App\Repositories\UserRepositoryInterface;
+use Facades\App\Repositories\UserRepositoryInterface;
 
 class UserController extends Controller
 {
-    private $userRepository;
-
-    public function __construct(UserRepositoryInterface $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
-
     public function index()
     {
-        return $this->userRepository->all();
+        return UserRepositoryInterface::all();
     }
 
     public function searchByName(SearchRequest $searchRequest)
     {
-        return $this->userRepository->searchByKey('name', $searchRequest->input('query_param'));
+        return UserRepositoryInterface::searchByKey('name', $searchRequest->input('query_param'));
     }
 
     public function store(UserRequest $request)
     {
-        return $this->userRepository->create($request->all());
+        return UserRepositoryInterface::create($request->all());
     }
 
     public function show($id)
     {
-        return $this->userRepository->findOrFail($id);
+        return UserRepositoryInterface::findOrFail($id);
     }
 
     public function update(UserUpdateRequest $request, $id)
     {
-        return $this->userRepository->update($id, $request->all());
+        return UserRepositoryInterface::update($id, $request->all());
     }
 
     public function destroy($id): bool
     {
-        return $this->userRepository->delete($id);
+        return UserRepositoryInterface::delete($id);
     }
 }
